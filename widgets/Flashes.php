bbba083b9f4ef91f72d925f92be2ccc4535f90ef
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-01-03 01:26:21 +0400

    New Messages component

---
+++ widgets/Flashes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace yz\admin\widgets;
+use yii\base\Widget;
+
+
+/**
+ * Class Flashes implements flash display for session's flashes
+ * @package \yz\admin\widgets
+ */
+class Flashes extends Widget
+{
+	public function run()
+	{
+		parent::run();
+	}
+
+} 
