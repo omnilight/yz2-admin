ea5faec88cdd45cd0d19fd2a699bf03d4fd816ff
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-01-08 19:36:22 +0400

    Changes

--- widgets/ActiveForm.php
+++ widgets/ActiveForm.php
@@ -10,49 +10,7 @@ use yz\admin\widgets\ActiveField;
  * Class ActiveForm
  * @package yz\admin\widgets
  */
-class ActiveForm extends \yii\widgets\ActiveForm
+class ActiveForm extends \yz\widgets\ActiveForm
 {
-	const TYPE_INLINE = 'form-inline';
-	const TYPE_VERTICAL = 'form-vertical';
-	const TYPE_HORIZONTAL = 'form-horizontal';
 
-	/**
-	 * Type of the form: TYPE_INLINE, TYPE_VERTICAL or TYPE_HORIZONTAL
-	 * @var string
-	 */
-	public $type = self::TYPE_HORIZONTAL;
-
-    public function init()
-    {
-        if (!isset($this->fieldConfig['class'])) {
-            $this->fieldConfig['class'] = ActiveField::className();
-        }
-
-		Html::addCssClass($this->options, static::$_configuration[$this->type]['class']);
-
-        parent::init();
-    }
-
-	public function run()
-	{
-		$view = $this->getView();
-		ActiveFormAsset::register($view);
-		parent::run();
-	}
-
-	/**
-	 * @inheritdoc
-	 * @return \yz\admin\widgets\ActiveField
-	 */
-	public function field($model, $attribute, $options = [])
-	{
-		return parent::field($model, $attribute, $options);
-	}
-
-
-	static protected $_configuration = [
-		self::TYPE_INLINE => ['class' => 'form-inline',],
-		self::TYPE_VERTICAL => ['class' => 'form-vertical',],
-		self::TYPE_HORIZONTAL => ['class' => 'form-horizontal',],
-	];
 }
