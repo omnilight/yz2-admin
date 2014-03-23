ae462ccb3fd8f5c040363d7c17203a6f9954c62b
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-03-23 19:01:10 +0400

    Changed Extension class to Bootstrap

---
+++ Bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace yz\admin;
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+
+/**
+ * Class Bootstrap
+ */
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param Application $app the application currently running
+     */
+    public function bootstrap(Application $app)
+    {
+        $app->i18n->translations['admin/*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'basePath' => '@yz/admin/messages',
+            'sourceLanguage' => 'en-US',
+        ];
+        $app->i18n->translations['admin/sysinfo'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'basePath' => '@yz/admin/messages',
+            'sourceLanguage' => 'en-US',
+        ];
+    }
+
+
+} 
