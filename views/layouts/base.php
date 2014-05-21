dfbca233a54431293f2d595f49f4fb3c2f963bbc
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-05-21 14:31:34 +0400

    Fixes

--- views/layouts/base.php
+++ views/layouts/base.php
@@ -17,7 +17,7 @@ Icons::register($this);
 <head>
     <title><?= Yii::t('admin/t','Administration panel'); ?></title>
     <meta charset="<?= Yii::$app->charset ?>"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=1008">
     <?php $this->head(); ?>
 </head>
 <body>
